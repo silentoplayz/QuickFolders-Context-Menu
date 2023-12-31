95fd00758d3d6bfa160cb4939575384a42868395
Silentoplayz <50341825+Silentoplayz@users.noreply.github.com>
2023-12-30 23:34:48 -0500

    Add files via upload
    Verbose Parameter in Write-Log: The Write-Log function now includes a Verbose switch parameter. When this switch is used, the log message is prefixed with "VERBOSE:" to indicate detailed logging.
    
    Verbose Logging in Functions: Functions like Add-QuickFoldersToContextMenu, Remove-QuickFoldersFromContextMenu, Remove-ExistingKey, Create-RegistryKey, and Add-SubMenuItems have been updated to include verbose logging. This will provide detailed information about each step, especially changes to the registry.
    
    User Prompt for Verbose Logging: The Prompt-UserAction function now calls the add/remove functions with the -Verbose switch to enable detailed logging.

--- QuickFolders.ps1
+++ QuickFolders.ps1
@@ -1,42 +1,59 @@
-# Function to Write Log
+# Function to Write Log with Verbose Option
 function Write-Log {
-    param ([string]$Message)
-    Write-Host $Message
+    param (
+        [string]$Message,
+        [switch]$Verbose
+    )
+    if ($Verbose) {
+        Write-Host "VERBOSE: $Message"
+    } else {
+        Write-Host $Message
+    }
 }
 
 # Function to Add QuickFolders to Context Menu
 function Add-QuickFoldersToContextMenu {
+    param (
+        [switch]$Verbose
+    )
     try {
         $quickFoldersPath = "HKCU:\Software\Classes\Directory\Background\shell\QuickFolders"
-        Remove-ExistingKey -Path $quickFoldersPath
+        Remove-ExistingKey -Path $quickFoldersPath -Verbose:$Verbose
         Create-RegistryKey -Path $quickFoldersPath -Properties @{
             "MUIVerb" = "QuickFolders"
             "SubCommands" = ""
-        }
+        } -Verbose:$Verbose
 
-        Add-SubMenuItems -QuickFoldersPath $quickFoldersPath
-        Write-Log "QuickFolders menu option added to the desktop context menu."
+        Add-SubMenuItems -QuickFoldersPath $quickFoldersPath -Verbose:$Verbose
+        Write-Log "QuickFolders menu option added to the desktop context menu." -Verbose:$Verbose
     } catch {
-        Write-Log "Error adding QuickFolders to context menu: $_"
+        Write-Log "Error adding QuickFolders to context menu: $_" -Verbose:$Verbose
     }
 }
 
 # Function to Remove QuickFolders from Context Menu
 function Remove-QuickFoldersFromContextMenu {
+    param (
+        [switch]$Verbose
+    )
     try {
         $quickFoldersPath = "HKCU:\Software\Classes\Directory\Background\shell\QuickFolders"
-        Remove-ExistingKey -Path $quickFoldersPath
-        Write-Log "QuickFolders menu option removed from the desktop context menu."
+        Remove-ExistingKey -Path $quickFoldersPath -Verbose:$Verbose
+        Write-Log "QuickFolders menu option removed from the desktop context menu." -Verbose:$Verbose
     } catch {
-        Write-Log "Error removing QuickFolders from context menu: $_"
+        Write-Log "Error removing QuickFolders from context menu: $_" -Verbose:$Verbose
     }
 }
 
 # Function to Remove Existing Registry Key
 function Remove-ExistingKey {
-    param ([string]$Path)
+    param (
+        [string]$Path,
+        [switch]$Verbose
+    )
     if (Test-Path $Path) {
         Remove-Item -Path $Path -Recurse
+        Write-Log "Removed registry key: $Path" -Verbose:$Verbose
     }
 }
 
@@ -44,26 +61,32 @@ function Remove-ExistingKey {
 function Create-RegistryKey {
     param (
         [string]$Path,
-        [hashtable]$Properties
+        [hashtable]$Properties,
+        [switch]$Verbose
     )
     New-Item -Path $Path -Force | Out-Null
+    Write-Log "Created registry key: $Path" -Verbose:$Verbose
     foreach ($key in $Properties.Keys) {
         Set-ItemProperty -Path $Path -Name $key -Value $Properties[$key]
+        Write-Log "Set property '$key' to '$($Properties[$key])' on '$Path'" -Verbose:$Verbose
     }
 }
 
 # Function to Add Sub-Menu Items
 function Add-SubMenuItems {
-    param ([string]$QuickFoldersPath)
+    param (
+        [string]$QuickFoldersPath,
+        [switch]$Verbose
+    )
     $desktopPath = [System.Environment]::GetFolderPath("Desktop")
     Get-ChildItem -Path $desktopPath -Directory | ForEach-Object {
         $subMenuPath = "$QuickFoldersPath\shell\$($_.Name)"
         Create-RegistryKey -Path $subMenuPath -Properties @{
             "MUIVerb" = $_.Name
-        }
+        } -Verbose:$Verbose
         Create-RegistryKey -Path "$subMenuPath\command" -Properties @{
             "(default)" = "explorer.exe `"$($_.FullName)`""
-        }
+        } -Verbose:$Verbose
     }
 }
 
@@ -74,11 +97,11 @@ function Prompt-UserAction {
         switch ($action.ToUpper()) {
             "A" {
                 $confirm = Read-Host "Are you sure you want to ADD QuickFolders to the context menu? (Y/N)"
-                if ($confirm -eq 'Y') { Add-QuickFoldersToContextMenu }
+                if ($confirm -eq 'Y') { Add-QuickFoldersToContextMenu -Verbose }
             }
             "R" {
                 $confirm = Read-Host "Are you sure you want to REMOVE QuickFolders from the context menu? (Y/N)"
-                if ($confirm -eq 'Y') { Remove-QuickFoldersFromContextMenu }
+                if ($confirm -eq 'Y') { Remove-QuickFoldersFromContextMenu -Verbose }
             }
             default { Write-Log "Invalid selection. Please enter 'A' to Add or 'R' to Remove."; Prompt-UserAction }
         }
